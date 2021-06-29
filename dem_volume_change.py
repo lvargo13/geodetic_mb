edc3045265f4c130a8e97907eca43e473973d66c
lvargo13 <lvargo13@gmail.com>
2021-06-29 12:11:00 +1200

    updated resolution and masking

--- dem_volume_change.py
+++ dem_volume_change.py
@@ -25,19 +25,24 @@ parser.add_argument('dem_1', help='dem of earlier year')
 parser.add_argument('dem_2', help='dem of later year')
 args = parser.parse_args()
 coregFlag = True   # True includes coregistration
+glacFlag = 'Brewster' # 'Rolleston or 'Brewster'
+if glacFlag == 'Rolleston':
+    odir = '/Volumes/arc_03/vargola/eoss_images/ROLLESTON/'
+    bedrx_poly_mask = odir+'bedrx_mask3.shp'
+else: 
+    odir = '/Volumes/arc_03/vargola/eoss_images/BREWSTER/'
+    bedrx_poly_mask = odir+'bedrx_poly.shp'
 
 # read in DEMs and project to same extents
 dem1 = GeoImg(args.dem_1) 
 dem2 = GeoImg(args.dem_2)
-odir = '/Volumes/arc_03/vargola/eoss_images/BREWSTER/'
+x_res = dem1.dx  # had to add this for calculating dv at the end, mask seems to distort 
+y_res = dem1.dy
 
 #coregister
 if coregFlag:
-    if os.path.isfile(args.dem_2[0:-4] + '_adj.tif'):
-        dem2 =GeoImg(args.dem_2[0:-4] + '_adj.tif')
-    else:
-        coreg.dem_coregistration(dem1,dem2,glaciermask=None,landmask=odir+'bedrx_poly.shp',outdir=odir,pts=False)
-        dem2 = GeoImg(args.dem_2[0:-4] + '_adj.tif')
+    coreg.dem_coregistration(dem1,dem2,glaciermask=None,landmask=bedrx_poly_mask,outdir=odir,pts=False)
+    dem2 = GeoImg(args.dem_2[0:-4] + '_adj.tif')
 
 # dh_dem
 dem2_reproj = dem2.reproject(dem1) # reproject master DEM to slave DEM extent, cell size
@@ -53,26 +58,32 @@ dh_bedrx.img[dh_bedrx.img > dem_rem] = np.nan
 dh_bedrx.img[dh_bedrx.img < (dem_rem*-1)] = np.nan
 
 # test coregistered bedrx error
-bedrx_mask = imtool.create_mask_from_shapefile(dh_dem,odir+'bedrx_poly.shp')
-dh_bedrx.mask(bedrx_mask,0)
+bedrx_ma = imtool.create_mask_from_shapefile(dem1,bedrx_poly_mask,buffer=None)
+bedrx_mask = np.invert(bedrx_ma) # inverting for mask
+dh_bedrx.img = np.ma.masked_where(bedrx_mask, dh_bedrx.img) # masking
+fig0 = dh_bedrx.display(fig=plt.figure(figsize=(8,8)), cmap='bwr_r', vmin=-10, vmax=10)
+fig0.gca().set_xlabel('utm easting (m)')
+fig0.gca().set_ylabel('utm northing (m)')
+cb = plt.colorbar(); cb.set_label('elevation difference (m)')
 #ts = np.ma.compressed(dh_bedrx.img)
 #plt.hist(ts,bins=40,range=[-7.5,7.5])
-#plt.show()
+#plt.show()  # to plot histogram
 
 # mask DEMs
-s1 = gpd.read_file("/Volumes/arc_03/vargola/eoss_images/BREWSTER/2016_outline1.shp")
-s2 = gpd.read_file("/Volumes/arc_03/vargola/eoss_images/BREWSTER/2019_outline.shp")
-s1arr = imtool.create_mask_from_shapefile(dh_dem,"/Volumes/arc_03/vargola/eoss_images/BREWSTER/2016_outline1.shp")
-dh_dem.mask(s1arr,0)  # mask the dh_dem using 2016 dem
+s1 = gpd.read_file(odir+'2016_outline1.shp')
+s2 = gpd.read_file(odir+'2021_outline.shp')
+s1arr_a = imtool.create_mask_from_shapefile(dem1,odir+'2016_outline1.shp',buffer=None)
+s1arr = np.invert(s1arr_a)
+dh_dem.img = np.ma.masked_where(s1arr, dh_dem.img) # masking
 
 # plot
-fig1 = dh_dem.display(fig=plt.figure(figsize=(8,8)), cmap='bwr_r', vmin=-15, vmax=15)
+fig1 = dh_dem.display(fig=plt.figure(figsize=(8,8)), cmap='bwr_r', vmin=-10, vmax=10)
 fig1.gca().set_xlabel('utm easting (m)')
 fig1.gca().set_ylabel('utm northing (m)')
 cb = plt.colorbar(); cb.set_label('elevation difference (m)')
 
 # calculate volume change
-dv = np.nansum(dh_dem.img) * np.abs(dh_dem.dx) * np.abs(dh_dem.dy)
+dv = np.nansum(dh_dem.img) * np.abs(x_res) * np.abs(y_res)
 s1_area = np.nansum(s1.area)
 s2_area = np.nansum(s2.area)
 smean = (s2_area+s1_area)/2
@@ -90,6 +101,3 @@ sig_area = 5  # huss hock uses 5%, we could estimate our own, should be lower
 sig = (sig_glac**2 + sig_geod**2 + sig_area**2)**0.5
 sig = B * (sig*0.01)  # need to convert back from percent
 print(sig)
-
-
-
